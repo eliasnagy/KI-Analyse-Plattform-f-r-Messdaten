1d596cf9c163699e0d6bba71b218b2b5c0576e00
Julian <lauberju@dhbw-loerrach.de>
2026-03-25 13:46:10 +0100

    bash fehler behoben

--- start.sh
+++ start.sh
@@ -8,11 +8,11 @@ read -p "Auswahl [1-2]: " wahl
 case $wahl in
   1)
     echo "Starte Torch-Programm..."
-    ./env_torch/bin/python3 dein_torch_skript.py
+    ./env_torch/bin/python3 torch/train.py
     ;;
   2)
     echo "Starte neues Programm..."
-    ./env_new/bin/python3 dein_neues_skript.py
+    ./env_new/bin/python3 klassische_modelle/train.py
     ;;
   *)
     echo "Ungültige Auswahl."
